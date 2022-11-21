ffffd878d7242b554adcc0b0d43d5788c166a7db
NenoPr <nenadpredan@gmail.com>
2022-11-21 18:36:54 +0100

    Changed the stucture of the projects component, added a universal component taht populets data from json. Added content and did some design in about section.

--- components/about.vue
+++ components/about.vue
@@ -1,19 +1,99 @@
 <template>
   <div class="about-container">
-    <h1 class="">About me</h1>
-    <p class="m-[1rem]">I dabble in proggramming mostly as a to keep the deterioration of my brain at bay from not doing anything with myself, only staying in my room every day, all day, rotting my brain with hate, anger, pity, rage, fear, porn, saddnes (though that is rare now), neglect.</p>
-    <div>Some technologies I use, and have worked with:</div>
-    <p>JavaScript</p>
-    <p>Python</p>
-    <p>C++</p>
-    <p>Git</p>
-    <p>React</p>
-    <p>Nuxt</p>
-    <p>Tailwind</p>
-    <p>Tensorflow</p>
-    <p>Keras</p>
-    <p>Pytorch</p>
-    <p>FastAi</p>
+    <div class="about-info">
+      <h1 class="about-header">About Neno</h1>
+      <img src="~/assets/images/profile_image.jpg" class="profile-image" />
+      <p class="profile-desc">
+        I dabble in programming mostly as to keep the deterioration of my
+        brain at bay from not doing anything with myself, only staying in my
+        room every day, all day, rotting my brain with hate, anger, pity, rage,
+        fear, porn, loneliness, sadness, neglect.
+      </p>
+    </div>
+    <div>
+      <div class="about-tools-header">
+        Some technologies I have worked with:
+      </div>
+      <div class="about-tools">
+        <div class="about-tool-holder">
+          <div>JavaScript</div>
+          <div class="about-tool-image">
+            <img src="~/assets/images/JavaScript_logo.png" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Python</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/python-icon.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>C++</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/c-plusplus.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Git</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/git-icon.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>React</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/react.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Nuxt</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/nuxt-icon.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Tailwind</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/tailwindcss-icon.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Tensorflow</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/tensorflow.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Keras</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/keras.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Pytorch</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/pytorch-icon.svg" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+        <div class="about-tool-holder">
+          <div>Fast.ai</div>
+          <div class="about-tool-image">
+            <img src="../assets/images/logos/Fast.ai.png" alt="" />
+          </div>
+        </div>
+        <!-- GAP -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,15 +101,77 @@
 
 <style scoped>
 .about-container {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 2rem;
   align-items: center;
-  padding: 2rem;
   border: 0.1rem solid rgb(199, 114, 114);
   border-radius: 0.5rem;
   border-top: rgb(199, 114, 114) solid 0.5rem;
   text-align: center;
   padding: 0.5rem;
+  padding-top: 3rem;
   width: 50%;
+}
+
+.about-header {
+  position: relative;
+  left: 50%;
+  bottom: 3rem;
+  font-weight: bold;
+  font-size: larger;
+}
+
+.about-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.profile-image {
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: 100%;
+  width: 15rem;
+  height: 15rem;
+}
+
+.profile-desc {
+  text-align: left;
+  width: 90%;
+}
+
+.about-tools-header {
+  position: relative;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+}
+
+.about-tools {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: auto;
+  justify-content: center;
+  align-content: center;
+  gap: 1rem;
+  padding: 1rem;
+}
+
+.about-tool-holder {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #656c82;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  gap: 0.5rem;
+  width: 5rem;
+}
+
+.about-tool-image {
+  width: 100%;
 }
 </style>
---
+++ components/projects/projectInfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <template v-for="project in projects">
+    <div class="project-holder">
+      <div class="project-details-name">{{ project.title, key }}</div>
+      <p class="project-details-description">
+        {{ project.desc }}
+      </p>
+      <p class="project-details-tool-header">Tools Used</p>
+      <div class="project-details-tools-holder">
+        <template v-for="item in project.tools" :key="item.tool">
+          <div class="project-details-tool">
+            <div class="project-details-tool-name">{{item.tool}}</div>
+            <div class="project-details-logo">
+              <img v-bind:src="'_nuxt/assets/images/' + item.image" :alt="item.tool" />
+            </div>
+          </div>
+        </template>
+      </div>
+      <a
+        :href="`${project.webLink}`"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="tryit-link"
+        >Try it Out</a
+      >
+      <img
+        v-bind:src="'_nuxt/assets/images/'+ project.image"
+        class="project-details-image"
+        alt=""
+      />
+    </div>
+    <div class="separator"></div>
+  </template>
+</template>
+
+<script setup>
+import projects from "~/assets/json/projects.json";
+
+
+console.log(projects);
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/projects.css";
+</style>

0c4f93a5c097afc5d00016aeda4487dd6fbb3f18
NenoPr <nenadpredan@gmail.com>
2022-11-25 02:15:49 +0100

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // mode: "static",
-  // router: {
-  //   base: "/portfolio-Top",
-  // },
+  mode: "static",
+  app: {
+    baseURL: "/portfolio-Top",
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
